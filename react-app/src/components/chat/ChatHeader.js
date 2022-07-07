ceffc34184bf48288b2f396c58fc9172b02d48ce
Josh Keim <joshuamkeim@gmail.com>
2022-07-06 20:56:31 -0400

    added heroku config

--- react-app/src/components/chat/ChatHeader.js
+++ react-app/src/components/chat/ChatHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ChatHeader() {
   return (
     <div className='chat-header'>
-        <h3><span className='hash'>#</span>Channel</h3>
+        
     </div>
   )
 }
--- react-app/src/components/chat/ChatPage.js
+++ react-app/src/components/chat/ChatPage.js
@@ -3,8 +3,22 @@ import ChatHeader from './ChatHeader'
 
 function Chat() {
   return (
-    <div className='chat-area'>
-      <ChatHeader />
+    <div className='message-container'>
+      <header className='chat-header'><span className='hash'>#</span>Channel</header>
+      <div className='message-content'>
+        <div className='view-messages-container'>
+        <div className='messages'>
+        <ul className='chat-messages'>
+          <li>A message</li>
+        </ul>
+        </div>
+        <div className='send-message-container'>
+              <form className='message-form'>
+                <input type='text' className='message-input' placeholder='type something' />
+              </form>
+          </div>
+      </div>
+      </div>
     </div>
   )
 }
